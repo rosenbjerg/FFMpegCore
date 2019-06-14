5292f9134d058260f85c029075352cd947981ec2
Trystan May <trystanmay@gmail.com>
2019-06-14 09:43:17 -0500

    Added test to test if IsWorking gets an exception.

--- FFMpegCore.Test/FFMpegTest.cs
+++ FFMpegCore.Test/FFMpegTest.cs
@@ -15,5 +15,28 @@ namespace FFMpegCore.Test
             Assert.IsNotNull(encoder);
             Assert.IsNotNull(probe);
         }
+        
+        [TestMethod]
+        public void TestWriteImageSequence()
+        {
+            var encoder = new FFMpeg();
+
+            var bitmapPaths = new List<string> {@".\temp\test-image-1.png", @".\temp\test-image-2.png"};
+
+
+            foreach (var path in bitmapPaths)
+            {
+                const int sideLength = 16;
+                var bitmap = new Bitmap(sideLength, sideLength);
+                var graphics = Graphics.FromImage(bitmap);
+                graphics.FillRectangle(Brushes.White, 0, 0, sideLength, sideLength);
+                graphics.DrawLine(Pens.Black, 0, 0, sideLength, sideLength);
+                bitmap.Save(path, ImageFormat.Png);
+            }
+
+            // This tests fails if this throws an exception.
+            encoder.JoinImageSequence(new FileInfo(@".\temp\test-image-sequence.mp4"),
+                images: bitmapPaths.Select(ImageInfo.FromPath).ToArray());
+        }
     }
 }
