170aefeb32513a9f7e2c4c4e8a1a8642de259908
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 18:28:49 +0100

    Support for crf

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/ConstantRateFactorArgument.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FFMpegCore.FFMPEG.Argument
+{
+    /// <summary>
+    /// Constant Rate Factor (CRF) argument
+    /// </summary>
+    public class ConstantRateFactorArgument : Argument<int>
+    {
+        public ConstantRateFactorArgument(int crf) : base(crf)
+        {
+            if (crf < 0 || crf > 63)
+            {
+                throw new ArgumentException("Argument is outside range (0 - 63)", nameof(crf));
+            }
+        }
+
+        public override string GetStringValue()
+        {
+            return $"-crf {Value} ";
+        }
+    }
+    /// <summary>
+    /// Constant Rate Factor (CRF) argument
+    /// </summary>
+    public class Variable : Argument<int>
+    {
+        public ConstantRateFactorArgument(int crf) : base(crf)
+        {
+            if (crf < 0 || crf > 63)
+            {
+                throw new ArgumentException("Argument is outside range (0 - 63)", nameof(crf));
+            }
+        }
+
+        public override string GetStringValue()
+        {
+            return $"-crf {Value} ";
+        }
+    }
+}
