2f54aa8eefb921f2ef417e5cc95a12bd3ccc74ab
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 18:29:09 +0100

    Add argument for removing metadata
    Former-commit-id: e688bcc674149c8ae023df6f5c81b19b580c61b3

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/RemoveMetadataArgument.cs
@@ -0,0 +1,17 @@
+﻿namespace FFMpegCore.FFMPEG.Argument
+{
+    /// <summary>
+    /// Remove metadata argument
+    /// </summary>
+    public class RemoveMetadataArgument : Argument
+    {
+        public RemoveMetadataArgument()
+        {
+        }
+
+        public override string GetStringValue()
+        {
+            return $"-map_metadata -1 ";
+        }
+    }
+}
