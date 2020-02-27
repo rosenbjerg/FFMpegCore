a5c0eb639c93c24464e6a2928e59d6303c81fe37
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 18:28:55 +0100

    Support for faststart
    Former-commit-id: b806f2bbfb58c1a40c7a10493896299d5b598300

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/FaststartArgument.cs
@@ -0,0 +1,17 @@
+﻿namespace FFMpegCore.FFMPEG.Argument
+{
+    /// <summary>
+    /// Faststart argument - for moving moov atom to the start of file
+    /// </summary>
+    public class FaststartArgument : Argument
+    {
+        public FaststartArgument()
+        {
+        }
+
+        public override string GetStringValue()
+        {
+            return "-movflags faststart ";
+        }
+    }
+}
