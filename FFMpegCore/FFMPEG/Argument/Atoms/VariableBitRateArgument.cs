3ff253f7440af6bfe46de8b5e5fb047a6cdd9295
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 18:31:57 +0100

    Add vbr support
    Former-commit-id: 098917a5da704b098ec249ef935d63d1e769e5a5

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/VariableBitRateArgument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FFMpegCore.FFMPEG.Argument
+{
+    /// <summary>
+    /// Variable Bitrate Argument (VBR) argument
+    /// </summary>
+    public class VariableBitRateArgument : Argument<int>
+    {
+        public VariableBitRateArgument(int vbr) : base(vbr)
+        {
+            if (vbr < 0 || vbr > 5)
+            {
+                throw new ArgumentException("Argument is outside range (0 - 5)", nameof(vbr));
+            }
+        }
+
+        public override string GetStringValue()
+        {
+            return $"-vbr {Value} ";
+        }
+    }
+}
