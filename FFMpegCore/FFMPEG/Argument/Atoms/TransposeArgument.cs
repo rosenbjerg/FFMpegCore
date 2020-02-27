21b35d349ed1fcfb5c79231fe69d4b06b4d4d5f5
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 21:48:15 +0100

    Add transpose argument

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/TransposeArgument.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FFMpegCore.FFMPEG.Argument
+{
+    /// <summary>
+    /// Transpose argument.
+    /// 0 = 90CounterCLockwise and Vertical Flip (default)
+    /// 1 = 90Clockwise
+    /// 2 = 90CounterClockwise
+    /// 3 = 90Clockwise and Vertical Flip
+    /// </summary>
+    public class TransposeArgument : Argument<int>
+    {
+        public TransposeArgument() { }
+
+        public TransposeArgument(int transpose) : base(transpose)
+        {
+            if (transpose < 0 || transpose > 5)
+            {
+                throw new ArgumentException("Argument is outside range (0 - 5)", nameof(transpose));
+            }
+        }
+
+        /// <inheritdoc/>
+        public override string GetStringValue()
+        {
+            return $"-vf \"transpose={Value}\"";
+        }
+    }
+}
