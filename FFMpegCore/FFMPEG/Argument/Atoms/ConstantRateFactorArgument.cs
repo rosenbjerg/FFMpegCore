616a6d1c7d37a9f8bd6a61fa297db09ac1d8f666
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-27 18:32:45 +0100

    Move to separate file
    Former-commit-id: 293ac4306af5ad705757937d001f258040144c50

--- FFMpegCore/FFMPEG/Argument/Atoms/ConstantRateFactorArgument.cs
+++ FFMpegCore/FFMPEG/Argument/Atoms/ConstantRateFactorArgument.cs
@@ -20,22 +20,4 @@ namespace FFMpegCore.FFMPEG.Argument
             return $"-crf {Value} ";
         }
     }
-    /// <summary>
-    /// Constant Rate Factor (CRF) argument
-    /// </summary>
-    public class Variable : Argument<int>
-    {
-        public ConstantRateFactorArgument(int crf) : base(crf)
-        {
-            if (crf < 0 || crf > 63)
-            {
-                throw new ArgumentException("Argument is outside range (0 - 63)", nameof(crf));
-            }
-        }
-
-        public override string GetStringValue()
-        {
-            return $"-crf {Value} ";
-        }
-    }
 }
