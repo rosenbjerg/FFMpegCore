cb0a5ac291450fd06dcf77a229f09b8886bd930f
weihanli <weihanli@outlook.com>
2020-04-28 12:26:42 +0800

    add RawArgument.cs
    Former-commit-id: 5643544a3b7ff082f84285df1014856f85a511c6

---
+++ FFMpegCore/FFMPEG/Argument/Atoms/RawArgument.cs
@@ -0,0 +1,14 @@
+﻿namespace FFMpegCore.FFMPEG.Argument
+{
+    public class RawArgument : Argument<string>
+    {
+        public RawArgument(string argument) : base(argument)
+        {
+        }
+
+        public override string GetStringValue()
+        {
+            return Value ?? string.Empty;
+        }
+    }
+}
