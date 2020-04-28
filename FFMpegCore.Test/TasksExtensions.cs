ea7acf2140258c87f08981cc1ffefe6db642b6a5
Максим Багрянцев <maxbag97619@gmail.com>
2020-04-28 16:08:02 +0300

    Updated TasksExtensions

--- FFMpegCore.Test/TasksExtensions.cs
+++ FFMpegCore.Test/TasksExtensions.cs
@@ -7,10 +7,7 @@ namespace FFMpegCore.Test
 {
     static class TasksExtensions
     {
-        public static T WaitForResult<T>(this Task<T> task)
-        {
-            task.Wait();
-            return task.Result;
-        }
+        public static T WaitForResult<T>(this Task<T> task) =>
+            task.ConfigureAwait(false).GetAwaiter().GetResult();
     }
 }
