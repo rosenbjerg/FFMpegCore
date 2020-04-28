e0582ef6c218001c97e1adef7cf86f0e2cb1c351
Максим Багрянцев <maxbag97619@gmail.com>
2020-04-28 18:39:39 +0300

    Added OutputPipeArgument
    Former-commit-id: 8434ffbba67e93ac917f5db14bf75822c190f5a7

---
+++ FFMpegCore/FFMPEG/Pipes/StreamPipeDataReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFMpegCore.FFMPEG.Pipes
+{
+    public class StreamPipedataReader : IPipeDataReader
+    {
+        public System.IO.Stream DestanationStream { get; private set; }
+        public int BlockSize { get; set; } = 4096;
+
+        public StreamPipedataReader(System.IO. Stream destanationStream)
+        {
+            DestanationStream = destanationStream;
+        }
+
+        public void ReadData(System.IO.Stream stream)
+        {
+            int read;
+            var buffer = new byte[BlockSize];
+            while((read = stream.Read(buffer, 0, buffer.Length)) != 0)
+                DestanationStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public async Task ReadDataAsync(System.IO.Stream stream)
+        {
+            int read;
+            var buffer = new byte[BlockSize];
+            while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                await DestanationStream.WriteAsync(buffer, 0, buffer.Length);
+        }
+    }
+}
