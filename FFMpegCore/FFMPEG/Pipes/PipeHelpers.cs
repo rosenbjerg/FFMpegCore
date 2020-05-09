056fb3e3e6d0024a82b869c09554df974ef07f3b
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-05-09 17:53:49 +0200

    Support piping on unix
    or at least trying
    
    
    Former-commit-id: eb9bc9807a2bd366dabfc71900bf7e5f70411da5

--- FFMpegCore/FFMPEG/Pipes/PipeHelpers.cs
+++ FFMpegCore/FFMPEG/Pipes/PipeHelpers.cs
@@ -10,7 +10,10 @@ namespace FFMpegCore.FFMPEG.Pipes
 
         public static string GetPipePath(string pipeName)
         {
-            return $@"\\.\pipe\{pipeName}";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return $@"\\.\pipe\{pipeName}";
+            else
+                return $"unix:/tmp/CoreFxPipe_{pipeName}"; // dotnet uses unix sockets on unix, for more see https://github.com/dotnet/runtime/issues/24390
         }
     }
 }
