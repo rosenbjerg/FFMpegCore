bbc9305e2b177c6f0aa151ea71a935c525411d9c
Максим Багрянцев <maxbag97619@gmail.com>
2020-05-12 17:44:14 +0300

    Added Codec, PixelFormat and ContainerFormat classes

---
+++ FFMpegCore/FFMpeg/Arguments/ForcePixelFormat.cs
@@ -0,0 +1,20 @@
+﻿using FFMpegCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFMpegCore.Arguments
+{
+    public class ForcePixelFormat : IArgument
+    {
+        public string PixelFormat { get; private set; }
+        public string Text => $"-pix_fmt {PixelFormat}";
+
+        public ForcePixelFormat(string format)
+        {
+            PixelFormat = format;
+        }
+
+        public ForcePixelFormat(PixelFormat format) : this(format.Name) { }
+    }
+}
