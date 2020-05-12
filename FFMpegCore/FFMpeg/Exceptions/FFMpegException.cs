08ace707cbe50c5fa513e8d123b493c23bdde877
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-05-12 17:55:31 +0200

    Default to throwing on errors
    Former-commit-id: 3855215000325f5ffb5e141cb7dbbe81962df22f

--- FFMpegCore/FFMpeg/Exceptions/FFMpegException.cs
+++ FFMpegCore/FFMpeg/Exceptions/FFMpegException.cs
@@ -13,14 +13,15 @@ namespace FFMpegCore.Exceptions
 
     public class FFMpegException : Exception
     {
-        public FFMpegException(FFMpegExceptionType type, string message): this(type, message, null) { }
 
-        public FFMpegException(FFMpegExceptionType type, string? message = null, Exception? innerException = null)
+        public FFMpegException(FFMpegExceptionType type, string? message = null, Exception? innerException = null, string ffMpegErrorOutput = "")
             : base(message, innerException)
         {
+            FFMpegErrorOutput = ffMpegErrorOutput;
             Type = type;
         }
 
         public FFMpegExceptionType Type { get; }
+        public string FFMpegErrorOutput { get; }
     }
 }
