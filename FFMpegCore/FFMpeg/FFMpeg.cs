00896ab11ac517352ce9871412677742a0060878
jianxingwu <jianxingwu@dj.cn>
2020-05-15 17:03:29 +0800

    FFMpeg.Join  error  Invalid output file. File extension should be 'mpegts' required.
    Former-commit-id: 738176479dbffd80b619ec3d4dd29c51e93bd5b3

--- FFMpegCore/FFMpeg/FFMpeg.cs
+++ FFMpegCore/FFMpeg/FFMpeg.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using FFMpegCore.Enums;
+﻿using FFMpegCore.Enums;
 using FFMpegCore.Exceptions;
 using FFMpegCore.Helpers;
 using FFMpegCore.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 
 namespace FFMpegCore
 {
