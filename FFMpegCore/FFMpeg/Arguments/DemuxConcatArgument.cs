4e5d464753659206d42394e8a41e2b4acc30a137
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-06-16 07:42:35 +0200

    Add demux concat

---
+++ FFMpegCore/FFMpeg/Arguments/DemuxConcatArgument.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FFMpegCore.Arguments
+{
+    /// <summary>
+    /// Represents parameter of concat argument
+    /// Used for creating video from multiple images or videos
+    /// </summary>
+    public class DemuxConcatArgument : IInputArgument
+    {
+        public readonly IEnumerable<string> Values;
+        public DemuxConcatArgument(IEnumerable<string> values)
+        {
+            Values = values;
+        }
+        private readonly string _tempFileName = Path.Combine(FFMpegOptions.Options.TempDirectory, Guid.NewGuid() + ".txt");
+
+        public void Pre()
+        {
+            File.WriteAllLines(_tempFileName, Values);
+        }
+
+        public void Post()
+        {
+            File.Delete(_tempFileName);
+        }
+
+        public string Text => $"-f concat -safe 0 -i \"{_tempFileName}\"";
+    }
+}
