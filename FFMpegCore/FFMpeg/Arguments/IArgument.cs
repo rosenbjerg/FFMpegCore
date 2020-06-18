4f3d2c1fda4073c7584ee912e22a20cb0d270b2c
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-06-18 21:36:59 +0200

    Move to separate files
    Former-commit-id: 552b5d811a5f3a2ba6d47e43d5bc6d8f5d25a74e

---
+++ FFMpegCore/FFMpeg/Arguments/IArgument.cs
@@ -0,0 +1,10 @@
+﻿namespace FFMpegCore.Arguments
+{
+    public interface IArgument
+    {
+        /// <summary>
+        /// The textual representation of the argument
+        /// </summary>
+        string Text { get; }
+    }
+}
