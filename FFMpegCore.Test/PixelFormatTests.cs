ecec9d2ea4ba006f4f3456004f48bb2f9dad29e9
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-07-20 15:26:34 +0200

    Clean unused using directives
    Former-commit-id: 8576154d3f71a2caacbfa695722f8b2803200f59

--- FFMpegCore.Test/PixelFormatTests.cs
+++ FFMpegCore.Test/PixelFormatTests.cs
@@ -1,8 +1,5 @@
 ﻿using FFMpegCore.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FFMpegCore.Test
 {
