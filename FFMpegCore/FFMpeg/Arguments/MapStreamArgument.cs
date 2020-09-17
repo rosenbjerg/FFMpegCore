b769f5aebccd8484c87f0bd29c4cf3223bcc83d8
Fedor Zhilkin <fzhilkin@mail.ru>
2020-09-17 20:50:38 +0300

    -map filtering
    Add map filtering to choose videostream
    
    
    Former-commit-id: e60fb71ff836e482915241309924e1b61f6b0802

---
+++ FFMpegCore/FFMpeg/Arguments/MapStreamArgument.cs
@@ -0,0 +1,17 @@
+﻿namespace FFMpegCore.Arguments
+{
+    /// <summary>
+    /// Represents choice of video stream, works with one input file
+    /// </summary>
+    public class MapStreamArgument : IArgument
+    {
+        public readonly int VideoStream;
+
+        public MapStreamArgument(int videoStreamNum)
+        {
+            VideoStream = videoStreamNum;
+        }
+
+        public string Text => $"-map 0:{VideoStream}";
+    }
+}
