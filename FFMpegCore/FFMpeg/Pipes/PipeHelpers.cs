3d50530c742bd5c3de39d6264a86f41b62ee1001
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-10-24 22:17:33 +0200

    Fix unix socket naming

--- FFMpegCore/FFMpeg/Pipes/PipeHelpers.cs
+++ FFMpegCore/FFMpeg/Pipes/PipeHelpers.cs
@@ -6,16 +6,14 @@ namespace FFMpegCore.Pipes
 {
     static class PipeHelpers
     {
-        static readonly string PipePrefix = Path.Combine(Path.GetTempPath(), "CoreFxPipe_");
-        
-        public static string GetUnqiuePipeName() => $"FFMpegCore_{Guid.NewGuid().ToString("N").Substring(0, 5)}";
+        public static string GetUnqiuePipeName() => $"FFMpegCore_{Guid.NewGuid()}";
 
         public static string GetPipePath(string pipeName)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return $@"\\.\pipe\{pipeName}";
             else
-                return $"unix:{PipePrefix}{pipeName}"; // dotnet uses unix sockets on unix, for more see https://github.com/dotnet/runtime/issues/24390
+                return $"unix:/run/CoreFxPipe_{pipeName}";
         }
     }
 }
