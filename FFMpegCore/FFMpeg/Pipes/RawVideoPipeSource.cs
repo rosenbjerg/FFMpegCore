fe646752d366a5a9c33fcc30ec457991241e6d41
Maxim Bagryantsev <maxbag97619@gmail.com>
2021-03-15 20:35:19 +0300

    Fixed process hang on pipe images format mismatch

--- FFMpegCore/FFMpeg/Pipes/RawVideoPipeSource.cs
+++ FFMpegCore/FFMpeg/Pipes/RawVideoPipeSource.cs
@@ -64,7 +64,7 @@ namespace FFMpegCore.Pipes
         private void CheckFrameAndThrow(IVideoFrame frame)
         {
             if (frame.Width != Width || frame.Height != Height || frame.Format != StreamFormat)
-                throw new FFMpegException(FFMpegExceptionType.Operation, "Video frame is not the same format as created raw video stream\r\n" +
+                throw new FFMpegStreamFormatException(FFMpegExceptionType.Operation, "Video frame is not the same format as created raw video stream\r\n" +
                     $"Frame format: {frame.Width}x{frame.Height} pix_fmt: {frame.Format}\r\n" +
                     $"Stream format: {Width}x{Height} pix_fmt: {StreamFormat}");
         }
