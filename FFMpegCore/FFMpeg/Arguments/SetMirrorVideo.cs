125643a7929bf319fad61ffaf8464d4e5c49b5de
Victor Horobchuk <Gorobchuk333@gmail.com>
2021-03-17 00:09:56 +0200

    Add mirror and default ForceFormat for OutputPipeArgument
    Former-commit-id: ec1b8a6b0757281825c4c9e869b698e8ada33206

---
+++ FFMpegCore/FFMpeg/Arguments/SetMirrorVideo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFMpegCore.Arguments
+{
+    public class SetMirrorVideo : IArgument
+    {
+        public string Text { get; set; } = "-vf \"hflip\"";
+    }
+}
