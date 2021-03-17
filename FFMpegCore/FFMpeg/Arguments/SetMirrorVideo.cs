d0afc5001b468f5889a30787ff141927d7efb05b
Victor Horobchuk <Gorobchuk333@gmail.com>
2021-03-17 09:58:01 +0200

    Update
    Former-commit-id: ab8be5cd57318d7b6e948afed23c136faa6d6a68

--- FFMpegCore/FFMpeg/Arguments/SetMirrorVideo.cs
+++ FFMpegCore/FFMpeg/Arguments/SetMirrorVideo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FFMpegCore.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,28 @@ namespace FFMpegCore.Arguments
 {
     public class SetMirrorVideo : IArgument
     {
-        public string Text { get; set; } = "-vf \"hflip\"";
+        public SetMirrorVideo(Mirror value)
+        {
+            _value = value;
+        }
+
+        public Mirror _value { get; set; }
+
+        public string Text
+        {
+            get
+            {
+                switch (_value)
+                {
+                    case Mirror.Horizontall:
+                        return "-vf \"hflip\"";
+                    case Mirror.Verticall:
+                        return "-vf \"vflip\"";
+                    default:
+                        throw new Exception("SetMirrorVideo: argument not found");
+                }
+
+            }
+        }
     }
 }
---
+++ FFMpegCore/FFMpeg/Enums/Mirror.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFMpegCore.Enums
+{
+    public enum Mirror : byte
+    {
+        Verticall = 1,
+        Horizontall = 2
+    }
+}
