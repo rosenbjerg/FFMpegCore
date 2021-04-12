d8810682efdfc63aa194d16f39211736cd3a514d
Julien Loir <mini.jul.jl@gmail.com>
2021-04-12 13:50:27 +0200

    Add simple support for PCM audio source wrapping

---
+++ FFMpegCore/FFMpeg/Pipes/IAudioSample.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFMpegCore.Pipes
+{
+    /// <summary>
+    /// Interface for Audio sample
+    /// </summary>
+    public interface IAudioSample
+    {
+        void Serialize(Stream stream);
+
+        Task SerializeAsync(Stream stream, CancellationToken token);
+    }
+}
---
+++ FFMpegCore/FFMpeg/Pipes/RawAudioPipeSource.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFMpegCore.Pipes
+{
+    /// <summary>
+    /// Implementation of <see cref="IPipeSource"/> for a raw audio stream that is gathered from <see cref="IEnumerator{IAudioFrame}"/> 
+    /// </summary>
+    public class RawAudioPipeSource : IPipeSource
+    {
+        private readonly IEnumerator<IAudioSample> _sampleEnumerator;
+
+        public string Format { get; set; } = "s16le";
+        public uint SampleRate { get; set; } = 8000;
+        public uint Channels { get; set; } = 1;
+
+        public RawAudioPipeSource(IEnumerator<IAudioSample> sampleEnumerator)
+        {
+            _sampleEnumerator = sampleEnumerator;
+        }
+
+        public RawAudioPipeSource(IEnumerable<IAudioSample> sampleEnumerator)
+            : this(sampleEnumerator.GetEnumerator()) { }
+
+        public string GetStreamArguments()
+        {
+            return $"-f {Format} -ar {SampleRate} -ac {Channels}";
+        }
+
+        public async Task WriteAsync(Stream outputStream, CancellationToken cancellationToken)
+        {
+            if (_sampleEnumerator.Current != null)
+            {
+                await _sampleEnumerator.Current.SerializeAsync(outputStream, cancellationToken).ConfigureAwait(false);
+            }
+
+            while (_sampleEnumerator.MoveNext())
+            {
+                await _sampleEnumerator.Current!.SerializeAsync(outputStream, cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
