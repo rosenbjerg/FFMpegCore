75386b6dacdd7a16492d4daa9598d6725b77f42f
alex6dj <alexeiagueroalba@gmail.com>
2021-08-05 15:11:23 -0400

    Document parameters
    Former-commit-id: 6247bf6ea4a0ce4bc74e1f93d4192419c27f3e7a

--- FFMpegCore/Extend/KeyValuePairExtensions.cs
+++ FFMpegCore/Extend/KeyValuePairExtensions.cs
@@ -4,6 +4,15 @@ namespace FFMpegCore.Extend
 {
     internal static class KeyValuePairExtensions
     {
+        /// <summary>
+        ///     Concat the two members of a <see cref="KeyValuePair{TKey,TValue}" />
+        /// </summary>
+        /// <param name="pair">Input object</param>
+        /// <param name="enclose">
+        ///     If true encloses the value part between quotes if contains an space character. If false use the
+        ///     value unmodified
+        /// </param>
+        /// <returns>The formatted string</returns>
         public static string FormatArgumentPair(this KeyValuePair<string, string> pair, bool enclose)
         {
             var key = pair.Key;
--- FFMpegCore/Extend/StringExtensions.cs
+++ FFMpegCore/Extend/StringExtensions.cs
@@ -2,6 +2,11 @@
 {
     internal static class StringExtensions
     {
+        /// <summary>
+        ///     Enclose string between quotes if contains an space character
+        /// </summary>
+        /// <param name="input">The input</param>
+        /// <returns>The enclosed string</returns>
         public static string EncloseIfContainsSpace(this string input)
         {
             return input.Contains(" ") ? $"'{input}'" : input;
