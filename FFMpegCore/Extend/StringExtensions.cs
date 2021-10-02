4a3c3abaa6d1ef529e3a6eac090cca0ec34e8944
alex6dj <alexeiagueroalba@gmail.com>
2021-10-01 23:34:56 -0400

    Fixed error when burning subtitle with some special charaters in path
    Former-commit-id: e6e07fc2fe0e5c480d31e211f4048818781a909c

--- FFMpegCore/Extend/StringExtensions.cs
+++ FFMpegCore/Extend/StringExtensions.cs
@@ -1,7 +1,19 @@
-﻿namespace FFMpegCore.Extend
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace FFMpegCore.Extend
 {
     internal static class StringExtensions
     {
+        private static Dictionary<char, string> CharactersSubstitution { get; } = new Dictionary<char, string>
+        {
+            {'\\', @"\\"},
+            {':', @"\:"},
+            {'[', @"\["},
+            {']', @"\]"},
+            // {'\'', @"\'"} TODO: Quotes need to be escaped but i failed miserably
+        };
+
         /// <summary>
         ///     Enclose string between quotes if contains an space character
         /// </summary>
@@ -10,6 +22,49 @@
         public static string EncloseIfContainsSpace(this string input)
         {
             return input.Contains(" ") ? $"'{input}'" : input;
+        }
+
+        /// <summary>
+        /// Enclose an string in quotes
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string EncloseInQuotes(this string input)
+        {
+            return $"'{input}'";
+        }
+
+        /// <summary>
+        ///     Scape several characters in subtitle path used by FFmpeg
+        /// </summary>
+        /// <remarks>
+        ///     This is needed because internally FFmpeg use Libav Filters
+        ///     and the info send to it must be in an specific format
+        /// </remarks>
+        /// <param name="source"></param>
+        /// <returns>Scaped path</returns>
+        public static string ToFFmpegLibavfilterPath(this string source)
+        {
+            return source.Replace(CharactersSubstitution);
+        }
+
+        public static string Replace(this string str, Dictionary<char, string> replaceList)
+        {
+            var parsedString = new StringBuilder();
+
+            foreach (var l in str)
+            {
+                if (replaceList.ContainsKey(l))
+                {
+                    parsedString.Append(replaceList[l]);
+                }
+                else
+                {
+                    parsedString.Append(l);
+                }
+            }
+
+            return parsedString.ToString();
         }
     }
 }
