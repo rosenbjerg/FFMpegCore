15fa6f099363b83da49f24bd7a989d3e0867ff69
alex6dj <alexeiagueroalba@gmail.com>
2021-10-04 13:05:44 -0400

    Document PanArgument
    Former-commit-id: 571cc88a3966f6e845af9babe60b1c8e9373ac9f

--- FFMpegCore/FFMpeg/Arguments/PanArgument.cs
+++ FFMpegCore/FFMpeg/Arguments/PanArgument.cs
@@ -4,13 +4,22 @@ using System.Linq;
 namespace FFMpegCore.Arguments
 {
     /// <summary>
-    /// Represents scale parameter
+    ///     Mix channels with specific gain levels.
     /// </summary>
     public class PanArgument : IAudioFilterArgument
     {
         public readonly string ChannelLayout;
         private readonly string[] _outputDefinitions;
 
+        /// <summary>
+        ///     Mix channels with specific gain levels <see href="https://ffmpeg.org/ffmpeg-filters.html#toc-pan-1"/>
+        /// </summary>
+        /// <param name="channelLayout">
+        ///     Represent the output channel layout. Like "stereo", "mono", "2.1", "5.1"
+        /// </param>
+        /// <param name="outputDefinitions">
+        ///     Output channel specification, of the form: "out_name=[gain*]in_name[(+-)[gain*]in_name...]"
+        /// </param>
         public PanArgument(string channelLayout, params string[] outputDefinitions)
         {
             if (string.IsNullOrWhiteSpace(channelLayout))
@@ -23,6 +32,13 @@ namespace FFMpegCore.Arguments
             _outputDefinitions = outputDefinitions;
         }
 
+        /// <summary>
+        ///     Mix channels with specific gain levels <see href="https://ffmpeg.org/ffmpeg-filters.html#toc-pan-1"/>
+        /// </summary>
+        /// <param name="channels">Number of channels in output file</param>
+        /// <param name="outputDefinitions">
+        ///     Output channel specification, of the form: "out_name=[gain*]in_name[(+-)[gain*]in_name...]"
+        /// </param>
         public PanArgument(int channels, params string[] outputDefinitions)
         {
             if (channels <= 0) throw new ArgumentOutOfRangeException(nameof(channels));
