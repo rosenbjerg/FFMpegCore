895a9842f5e5ef88d7c1c52ee56cbfe869695b01
Weirdo <git.jonas@kamsker.at>
2021-12-21 00:10:59 +0100

    Added MetaDataArgument
    Former-commit-id: e3eb2f2056ec7ccb0b36d4c2136e4fac4743ccbf

---
+++ FFMpegCore/FFMpeg/Arguments/MetaDataArgument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFMpegCore.Arguments
+{
+    public class MetaDataArgument : IInputArgument
+    {
+        private readonly string _metaDataContent;
+        private readonly string _tempFileName = Path.Combine(GlobalFFOptions.Current.TemporaryFilesFolder, $"metadata_{Guid.NewGuid()}.txt");
+
+        public MetaDataArgument(string metaDataContent)
+        {
+            _metaDataContent = metaDataContent;
+        }
+
+        public string Text => $"-i \"{_tempFileName}\" -map_metadata 1";
+
+        public Task During(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+
+        public void Pre() => File.WriteAllText(_tempFileName, _metaDataContent);
+
+        public void Post() => File.Delete(_tempFileName);
+    }
+}
