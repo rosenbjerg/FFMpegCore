25a7bea2f1184ba77faead430f006273dbe9d128
Weirdo <git.jonas@kamsker.at>
2021-12-21 00:34:33 +0100

    Added MetadataBuilder
    Former-commit-id: 2605ac1a54dcc5b76325bcc4045bc20e4b64fba4

---
+++ FFMpegCore/FFMpeg/Builders/MetaData/IReadOnlyMetaData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FFMpegCore.Builders.MetaData
+{
+
+    public interface IReadOnlyMetaData
+    {
+        IReadOnlyList<ChapterData> Chapters { get; }
+        IReadOnlyDictionary<string, string> Entries { get; }
+    }
+}
---
+++ FFMpegCore/FFMpeg/Builders/MetaData/MetaDataSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace FFMpegCore.Builders.MetaData
+{
+    public class MetaDataSerializer
+    {
+        public static readonly MetaDataSerializer Instance = new MetaDataSerializer();
+
+        public string Serialize(IReadOnlyMetaData metaData)
+        {
+            var sb = new StringBuilder()
+                .AppendLine(";FFMETADATA1");
+
+            foreach (var value in metaData.Entries)
+            {
+                sb.AppendLine($"{value.Key}={value.Value}");
+            }
+
+            int chapterNumber = 0;
+            foreach (var chapter in metaData.Chapters ?? Enumerable.Empty<ChapterData>())
+            {
+                chapterNumber++;
+                var title = string.IsNullOrEmpty(chapter.Title) ? $"Chapter {chapterNumber}" : chapter.Title;
+
+                sb
+                    .AppendLine("[CHAPTER]")
+                    .AppendLine($"TIMEBASE=1/1000")
+                    .AppendLine($"START={(int)chapter.Start.TotalMilliseconds}")
+                    .AppendLine($"END={(int)chapter.End.TotalMilliseconds}")
+                    .AppendLine($"title={title}")
+                    ;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
