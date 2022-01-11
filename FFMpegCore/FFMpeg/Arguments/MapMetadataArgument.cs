7e6738edafe290683e0239b76939302f86c2f26b
Jonas Kamsker <git.jonas@kamsker.at>
2022-01-12 00:15:32 +0100

    Added support for aaxc decryption
    Former-commit-id: 2626b3796b1d37dee945dd0e9a63a29bd5d55664

---
+++ FFMpegCore/FFMpeg/Arguments/MapMetadataArgument.cs
@@ -0,0 +1,44 @@
+﻿using FFMpegCore.Extend;
+
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFMpegCore.Arguments
+{
+    public class MapMetadataArgument : IInputArgument, IDynamicArgument
+    {
+        private readonly int? _inputIndex;
+
+        /// <summary>
+        /// Null means it takes the last input used befroe this argument
+        /// </summary>
+        /// <param name="inputIndex"></param>
+        public MapMetadataArgument(int? inputIndex = null)
+        {
+            _inputIndex = inputIndex;
+        }
+
+        public string Text => GetText(null);
+
+        public string GetText(StringBuilder context)
+        {
+            var index = _inputIndex ?? context?.ToString().CountOccurrences("-i") -1 ?? 0;
+            return $"-map_metadata {index}";
+        }
+
+
+        public Task During(CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Post()
+        {
+        }
+
+        public void Pre()
+        {
+        }
+    }
+}
