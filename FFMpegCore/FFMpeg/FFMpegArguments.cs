dd2bfac8401588417259cafdd442679495d74357
Jonas Kamsker <git.jonas@kamsker.at>
2022-01-11 22:42:31 +0100

    Using correct map_metadata dynamically
    Former-commit-id: 1b2af5fd1f8f9347a8cda058fe3eb9ac5cace1a7

--- FFMpegCore/FFMpeg/FFMpegArguments.cs
+++ FFMpegCore/FFMpeg/FFMpegArguments.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
 using FFMpegCore.Arguments;
 using FFMpegCore.Builders.MetaData;
 using FFMpegCore.Pipes;
@@ -13,10 +15,26 @@ namespace FFMpegCore
     public sealed class FFMpegArguments : FFMpegArgumentsBase
     {
         private readonly FFMpegGlobalArguments _globalArguments = new FFMpegGlobalArguments();
-        
+
         private FFMpegArguments() { }
 
-        public string Text => string.Join(" ", _globalArguments.Arguments.Concat(Arguments).Select(arg => arg.Text));
+        public string Text => GetText();
+
+        private string GetText()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var arg in _globalArguments.Arguments.Concat(Arguments))
+            {
+                if (sb.Length != 0)
+                {
+                    sb.Append(' ');
+                }
+                sb.Append(arg is IDynamicArgument dynArg ? dynArg.GetText(sb) : arg.Text);
+            }
+
+            return sb.ToString();
+        }
 
         public static FFMpegArguments FromConcatInput(IEnumerable<string> filePaths, Action<FFMpegArgumentOptions>? addArguments = null) => new FFMpegArguments().WithInput(new ConcatArgument(filePaths), addArguments);
         public static FFMpegArguments FromDemuxConcatInput(IEnumerable<string> filePaths, Action<FFMpegArgumentOptions>? addArguments = null) => new FFMpegArguments().WithInput(new DemuxConcatArgument(filePaths), addArguments);
@@ -26,7 +44,7 @@ namespace FFMpegCore
         public static FFMpegArguments FromDeviceInput(string device, Action<FFMpegArgumentOptions>? addArguments = null) => new FFMpegArguments().WithInput(new InputDeviceArgument(device), addArguments);
         public static FFMpegArguments FromPipeInput(IPipeSource sourcePipe, Action<FFMpegArgumentOptions>? addArguments = null) => new FFMpegArguments().WithInput(new InputPipeArgument(sourcePipe), addArguments);
 
-        
+
         public FFMpegArguments WithGlobalOptions(Action<FFMpegGlobalArguments> configureOptions)
         {
             configureOptions(_globalArguments);
