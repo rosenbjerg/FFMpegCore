33757acc87e377119fbe6acc4fe0a87a4ecce348
Jonas Kamsker <git.jonas@kamsker.at>
2022-01-12 00:15:32 +0100

    Added support for aaxc decryption
    Former-commit-id: 2626b3796b1d37dee945dd0e9a63a29bd5d55664

---
+++ FFMpegCore/FFMpeg/Arguments/AudibleEncryptionKeyArgument.cs
@@ -0,0 +1,16 @@
+﻿namespace FFMpegCore.Arguments
+{
+    public class AudibleEncryptionKeyArgument : IArgument
+    {
+        private readonly string _key;
+        private readonly string _iv;
+
+        public AudibleEncryptionKeyArgument(string key, string iv)
+        {
+            _key = key;
+            _iv = iv;
+        }
+
+        public string Text => $"-audible_key {_key} -audible_iv {_iv}";
+    }
+}
