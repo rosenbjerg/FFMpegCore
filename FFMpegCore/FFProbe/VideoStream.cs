2133d310217f760678fa28c8f5b8409b8e5d9c21
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2022-03-24 20:27:09 +0100

    Expose avg_frame_rate as AverageFrameRate on VideoStream #300

--- FFMpegCore/FFProbe/VideoStream.cs
+++ FFMpegCore/FFProbe/VideoStream.cs
@@ -13,6 +13,7 @@ namespace FFMpegCore
         public double FrameRate { get; internal set; }
         public string PixelFormat { get; internal set; } = null!;
         public int Rotation { get; set; }
+        public double AverageFrameRate { get; set; }
 
         public PixelFormat GetPixelFormatInfo() => FFMpeg.GetPixelFormat(PixelFormat);
     }
