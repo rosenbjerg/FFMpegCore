6e4e8fbba1ac6c3bddd5de2da8b6fe0284af8d1d
Thodoris Koskinopoulos <me@koskit.me>
2022-11-27 05:55:00 +0200

    Added log levels

---
+++ FFMpegCore/FFMpeg/Enums/FFMpegLogLevel.cs
@@ -0,0 +1,15 @@
+﻿namespace FFMpegCore.Enums
+{
+    public enum FFMpegLogLevel
+    {
+        Quiet = 0,
+        Panic = 1,
+        Fatal = 2,
+        Error = 3,
+        Warning = 4,
+        Info = 5,
+        Verbose = 6,
+        Debug = 7,
+        Trace = 8
+    }
+}
