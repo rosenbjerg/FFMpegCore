7f72625eacd3efb6e53605e9b3471402edc77850
Thodoris Koskinopoulos <me@koskit.me>
2022-11-27 05:54:29 +0200

    Added blackdetect and blackframe arguments

---
+++ FFMpegCore/FFMpeg/Arguments/BlackDetectArgument.cs
@@ -0,0 +1,14 @@
+﻿namespace FFMpegCore.Arguments
+{
+    public class BlackDetectArgument : IVideoFilterArgument
+    {
+        public string Key => "blackdetect";
+
+        public string Value { get; }
+
+        public BlackDetectArgument(double minimumDuration = 2.0, double pictureBlackRatioThreshold = 0.98, double pixelBlackThreshold = 0.1)
+        {
+            Value = $"d={minimumDuration}:pic_th={pictureBlackRatioThreshold}:pix_th={pixelBlackThreshold}";
+        }
+    }
+}
