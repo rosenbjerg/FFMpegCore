9b5f8dee6a0ee73acbf9da82a72e63f850ce778d
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2023-02-01 23:50:36 +0100

    Merge boolean conditions into pattern

--- FFMpegCore/FFMpeg/Arguments/AudioGateArgument.cs
+++ FFMpegCore/FFMpeg/Arguments/AudioGateArgument.cs
@@ -9,7 +9,7 @@ namespace FFMpegCore.Arguments
         /// <summary>
         ///     Audio Gate. <see href="https://ffmpeg.org/ffmpeg-filters.html#agate"/>
         /// </summary>
-        /// <param name="level_in">Set input level before filtering. Default is 1. Allowed range is from 0.015625 to 64.</param>
+        /// <param name="levelIn">Set input level before filtering. Default is 1. Allowed range is from 0.015625 to 64.</param>
         /// <param name="mode">Set the mode of operation. Can be upward or downward. Default is downward. If set to upward mode, higher parts of signal will be amplified, expanding dynamic range in upward direction. Otherwise, in case of downward lower parts of signal will be reduced.</param>
         /// <param name="range">Set the level of gain reduction when the signal is below the threshold. Default is 0.06125. Allowed range is from 0 to 1. Setting this to 0 disables reduction and then filter behaves like expander.</param>
         /// <param name="threshold">If a signal rises above this level the gain reduction is released. Default is 0.125. Allowed range is from 0 to 1.</param>
@@ -20,11 +20,12 @@ namespace FFMpegCore.Arguments
         /// <param name="knee">Curve the sharp knee around the threshold to enter gain reduction more softly. Default is 2.828427125. Allowed range is from 1 to 8.</param>
         /// <param name="detection">Choose if exact signal should be taken for detection or an RMS like one. Default is rms. Can be peak or rms.</param>
         /// <param name="link">Choose if the average level between all channels or the louder channel affects the reduction. Default is average. Can be average or maximum.</param>
-        public AudioGateArgument(double level_in = 1, string mode = "downward", double range = 0.06125, double threshold = 0.125, int ratio = 2, double attack = 20, double release = 250, int makeup = 1, double knee = 2.828427125, string detection = "rms", string link = "average")
+        public AudioGateArgument(double levelIn = 1, string mode = "downward", double range = 0.06125, double threshold = 0.125, int ratio = 2,
+            double attack = 20, double release = 250, int makeup = 1, double knee = 2.828427125, string detection = "rms", string link = "average")
         {
-            if (level_in < 0.015625 || level_in > 64)
+            if (levelIn is < 0.015625 or > 64)
             {
-                throw new ArgumentOutOfRangeException(nameof(level_in), "Level in must be between 0.015625 to 64");
+                throw new ArgumentOutOfRangeException(nameof(levelIn), "Level in must be between 0.015625 to 64");
             }
 
             if (mode != "upward" && mode != "downward")
@@ -32,37 +33,37 @@ namespace FFMpegCore.Arguments
                 throw new ArgumentOutOfRangeException(nameof(mode), "Mode must be either upward or downward");
             }
 
-            if (range <= 0 || range > 1)
+            if (range is <= 0 or > 1)
             {
                 throw new ArgumentOutOfRangeException(nameof(range));
             }
 
-            if (threshold < 0 || threshold > 1)
+            if (threshold is < 0 or > 1)
             {
                 throw new ArgumentOutOfRangeException(nameof(threshold), "Threshold must be between 0 and 1");
             }
 
-            if (ratio < 1 || ratio > 9000)
+            if (ratio is < 1 or > 9000)
             {
                 throw new ArgumentOutOfRangeException(nameof(ratio), "Ratio must be between 1 and 9000");
             }
 
-            if (attack < 0.01 || attack > 9000)
+            if (attack is < 0.01 or > 9000)
             {
                 throw new ArgumentOutOfRangeException(nameof(attack), "Attack must be between 0.01 and 9000");
             }
 
-            if (release < 0.01 || release > 9000)
+            if (release is < 0.01 or > 9000)
             {
                 throw new ArgumentOutOfRangeException(nameof(release), "Release must be between 0.01 and 9000");
             }
 
-            if (makeup < 1 || makeup > 64)
+            if (makeup is < 1 or > 64)
             {
                 throw new ArgumentOutOfRangeException(nameof(makeup), "Makeup Gain must be between 1 and 64");
             }
 
-            if (knee < 1 || knee > 64)
+            if (knee is < 1 or > 64)
             {
                 throw new ArgumentOutOfRangeException(nameof(makeup), "Knee must be between 1 and 8");
             }
@@ -77,7 +78,7 @@ namespace FFMpegCore.Arguments
                 throw new ArgumentOutOfRangeException(nameof(link), "Link must be either average or maximum");
             }
 
-            _arguments.Add("level_in", level_in.ToString("0.00", CultureInfo.InvariantCulture));
+            _arguments.Add("level_in", levelIn.ToString("0.00", CultureInfo.InvariantCulture));
             _arguments.Add("mode", mode);
             _arguments.Add("range", range.ToString("0.00", CultureInfo.InvariantCulture));
             _arguments.Add("threshold", threshold.ToString("0.00", CultureInfo.InvariantCulture));
