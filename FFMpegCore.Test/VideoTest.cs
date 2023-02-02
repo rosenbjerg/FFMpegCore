ae02f6a564e68598cb498851fdae82038f98c265
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2023-02-02 20:36:31 +0100

    Fix expected seconds in Video_Join_Image_Sequence

--- FFMpegCore.Test/VideoTest.cs
+++ FFMpegCore.Test/VideoTest.cs
@@ -466,7 +466,7 @@ namespace FFMpegCore.Test
             Assert.IsTrue(success);
             var result = FFProbe.Analyse(outputFile);
 
-            Assert.AreEqual(3, result.Duration.Seconds);
+            Assert.AreEqual(1, result.Duration.Seconds);
             Assert.AreEqual(imageAnalysis.PrimaryVideoStream!.Width, result.PrimaryVideoStream!.Width);
             Assert.AreEqual(imageAnalysis.PrimaryVideoStream!.Height, result.PrimaryVideoStream.Height);
         }
