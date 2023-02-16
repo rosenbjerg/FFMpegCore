cc22d15061ac62922ccb120729864aa5a4f1619a
Dimitri Vranken <dimitri.vranken@adesso.ch>
2023-02-16 09:37:01 +0100

    Moved SkiaSharp implemented to its own extension

---
+++ FFMpegCore.Extensions.SkiaSharp/BitmapVideoFrameWrapper.cs
@@ -0,0 +1,58 @@
+﻿using FFMpegCore.Pipes;
+using SkiaSharp;
+
+namespace FFMpegCore.Extensions.SkiaSharp
+{
+    public class BitmapVideoFrameWrapper : IVideoFrame, IDisposable
+    {
+        public int Width => Source.Width;
+
+        public int Height => Source.Height;
+
+        public string Format { get; private set; }
+
+        public SKBitmap Source { get; private set; }
+
+        public BitmapVideoFrameWrapper(SKBitmap bitmap)
+        {
+            Source = bitmap ?? throw new ArgumentNullException(nameof(bitmap));
+            Format = ConvertStreamFormat(bitmap.ColorType);
+        }
+
+        public void Serialize(Stream stream)
+        {
+            var data = Source.Bytes;
+            stream.Write(data, 0, data.Length);
+        }
+
+        public async Task SerializeAsync(Stream stream, CancellationToken token)
+        {
+            var data = Source.Bytes;
+            await stream.WriteAsync(data, 0, data.Length, token).ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            Source.Dispose();
+        }
+
+        private static string ConvertStreamFormat(SKColorType fmt)
+        {
+            switch (fmt)
+            {
+                case SKColorType.Gray8:
+                    return "gray8";
+                case SKColorType.Bgra8888:
+                    return "bgra";
+                case SKColorType.Rgb888x:
+                    return "rgb";
+                case SKColorType.Rgba8888:
+                    return "rgba";
+                case SKColorType.Rgb565:
+                    return "rgb565";
+                default:
+                    throw new NotSupportedException($"Not supported pixel format {fmt}");
+            }
+        }
+    }
+}
--- FFMpegCore.Extensions.System.Drawing.Common/BitmapVideoFrameWrapper.cs
+++ FFMpegCore.Extensions.System.Drawing.Common/BitmapVideoFrameWrapper.cs
@@ -1,5 +1,7 @@
-﻿using FFMpegCore.Pipes;
-using SkiaSharp;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using FFMpegCore.Pipes;
 
 namespace FFMpegCore.Extensions.System.Drawing.Common
 {
@@ -11,24 +13,44 @@ namespace FFMpegCore.Extensions.System.Drawing.Common
 
         public string Format { get; private set; }
 
-        public SKBitmap Source { get; private set; }
+        public Bitmap Source { get; private set; }
 
-        public BitmapVideoFrameWrapper(SKBitmap bitmap)
+        public BitmapVideoFrameWrapper(Bitmap bitmap)
         {
             Source = bitmap ?? throw new ArgumentNullException(nameof(bitmap));
-            Format = ConvertStreamFormat(bitmap.ColorType);
+            Format = ConvertStreamFormat(bitmap.PixelFormat);
         }
 
         public void Serialize(Stream stream)
         {
-            var data = Source.Bytes;
-            stream.Write(data, 0, data.Length);
+            var data = Source.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadOnly, Source.PixelFormat);
+
+            try
+            {
+                var buffer = new byte[data.Stride * data.Height];
+                Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            finally
+            {
+                Source.UnlockBits(data);
+            }
         }
 
         public async Task SerializeAsync(Stream stream, CancellationToken token)
         {
-            var data = Source.Bytes;
-            await stream.WriteAsync(data, 0, data.Length, token).ConfigureAwait(false);
+            var data = Source.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadOnly, Source.PixelFormat);
+
+            try
+            {
+                var buffer = new byte[data.Stride * data.Height];
+                Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+                await stream.WriteAsync(buffer, 0, buffer.Length, token).ConfigureAwait(false);
+            }
+            finally
+            {
+                Source.UnlockBits(data);
+            }
         }
 
         public void Dispose()
@@ -36,20 +58,27 @@ namespace FFMpegCore.Extensions.System.Drawing.Common
             Source.Dispose();
         }
 
-        private static string ConvertStreamFormat(SKColorType fmt)
+        private static string ConvertStreamFormat(PixelFormat fmt)
         {
             switch (fmt)
             {
-                case SKColorType.Gray8:
-                    return "gray8";
-                case SKColorType.Bgra8888:
+                case PixelFormat.Format16bppGrayScale:
+                    return "gray16le";
+                case PixelFormat.Format16bppRgb555:
+                    return "bgr555le";
+                case PixelFormat.Format16bppRgb565:
+                    return "bgr565le";
+                case PixelFormat.Format24bppRgb:
+                    return "bgr24";
+                case PixelFormat.Format32bppArgb:
                     return "bgra";
-                case SKColorType.Rgb888x:
-                    return "rgb";
-                case SKColorType.Rgba8888:
+                case PixelFormat.Format32bppPArgb:
+                    //This is not really same as argb32
+                    return "argb";
+                case PixelFormat.Format32bppRgb:
                     return "rgba";
-                case SKColorType.Rgb565:
-                    return "rgb565";
+                case PixelFormat.Format48bppRgb:
+                    return "rgb48le";
                 default:
                     throw new NotSupportedException($"Not supported pixel format {fmt}");
             }
