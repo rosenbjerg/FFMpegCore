349b6044d16efe016911f1cd27757463343b0b9d
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2023-02-23 19:18:34 +0100

    Wrap Instances exception for expected behaviour

--- FFMpegCore.Test/FFMpegArgumentProcessorTest.cs
+++ FFMpegCore.Test/FFMpegArgumentProcessorTest.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using FFMpegCore.Arguments;
+using FFMpegCore.Exceptions;
+using FFMpegCore.Helpers;
 using FluentAssertions;
+using Instances.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FFMpegCore.Test
@@ -98,6 +101,13 @@ namespace FFMpegCore.Test
         {
             var arg = new AudibleEncryptionKeyArgument("62689101");
             arg.Text.Should().Be($"-activation_bytes 62689101");
+        }
+
+        [TestMethod]
+        public void Throws_FFMpegException_when_ffmpeg_not_found()
+        {
+            var exception = Assert.ThrowsException<FFMpegException>(() => FFMpegHelper.VerifyFFMpegExists(new FFOptions { BinaryFolder = Path.GetTempPath() }));
+            Assert.IsInstanceOfType<InstanceFileNotFoundException>(exception.InnerException);
         }
     }
 }
