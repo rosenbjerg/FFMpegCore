19c177a248d7a627671b2162534089446855fc6b
Rafael Carvalho <rpaschoal@live.com>
2023-03-07 16:19:03 +1300

    Receive stream index as an argument for generating GIFs

--- FFMpegCore/FFMpeg/Arguments/GifPalettArgument.cs
+++ FFMpegCore/FFMpeg/Arguments/GifPalettArgument.cs
@@ -4,18 +4,21 @@ namespace FFMpegCore.Arguments
 {
     public class GifPalettArgument : IArgument
     {
+        private readonly int _streamIndex;
+
         private readonly int _fps;
 
         private readonly Size? _size;
 
-        public GifPalettArgument(int fps, Size? size)
+        public GifPalettArgument(int streamIndex, int fps, Size? size)
         {
+            _streamIndex = streamIndex;
             _fps = fps;
             _size = size;
         }
 
         private string ScaleText => _size.HasValue ? $"scale=w={_size.Value.Width}:h={_size.Value.Height}," : string.Empty;
 
-        public string Text => $"-filter_complex \"[0:v] fps={_fps},{ScaleText}split [a][b];[a] palettegen=max_colors=32 [p];[b][p] paletteuse=dither=bayer\"";
+        public string Text => $"-filter_complex \"[{_streamIndex}:v] fps={_fps},{ScaleText}split [a][b];[a] palettegen=max_colors=32 [p];[b][p] paletteuse=dither=bayer\"";
     }
 }
