6524b1cd4d90f1d204c359823045f20300d546ab
K <28016308+yuqian5@users.noreply.github.com>
2023-10-10 15:59:11 -0600

    Update DownloaderTests.cs

--- FFMpegCore.Test/DownloaderTests.cs
+++ FFMpegCore.Test/DownloaderTests.cs
@@ -6,21 +6,17 @@ namespace FFMpegCore.Test;
 [TestClass]
 public class DownloaderTests
 {
-    [Ignore]
     [TestMethod]
     public void GetAllLatestSuiteTest()
     {
-        var binaries = FFMpegDownloader.DownloadFFMpegSuite(binaries: FFMpegDownloader.FFMpegBinaries.FFProbe |
-                                                                      FFMpegDownloader.FFMpegBinaries.FFMpeg |
-                                                                      FFMpegDownloader.FFMpegBinaries.FFPlay).Result;
-        Assert.IsTrue(binaries.Count >= 2); // many platforms have only ffmpeg and ffprobe
+        var binaries = FFMpegDownloader.DownloadFFMpegSuite(binaries: FFMpegDownloader.FFMpegBinaries.FFMpeg).Result;
+        Assert.IsTrue(binaries.Count == 1); // many platforms have only ffmpeg and ffprobe
     }
 
-    [Ignore]
     [TestMethod]
     public void GetSpecificVersionTest()
     {
-        var binaries = FFMpegDownloader.DownloadFFMpegSuite(FFMpegDownloader.FFMpegVersions.V4_0).Result;
-        Assert.IsTrue(binaries.Count == 2);
+        var binaries = FFMpegDownloader.DownloadFFMpegSuite(FFMpegDownloader.FFMpegVersions.V4_0, binaries: FFMpegDownloader.FFMpegBinaries.FFMpeg).Result;
+        Assert.IsTrue(binaries.Count == 1);
     }
 }
