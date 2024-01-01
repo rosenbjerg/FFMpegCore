36d778a3a24da06f95eeef439499f3d2fe1bb083
Omri Goldberger <omri.goldberger@ludeo.com>
2024-01-01 10:47:26 +0200

    Added a support for pre-exsiting pipe

---
+++ FFMpegCore/FFMpeg/Arguments/InputExistingPipeArgument.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using FFMpegCore.Pipes;
+
+namespace FFMpegCore.Arguments
+{
+    /// <summary>
+    /// Represents input parameter for a specific name pipe
+    /// </summary>
+    public class InputExistingPipeArgument : IInputArgument
+    {
+        public string PipeName { get; }
+        public string PipePath => PipeHelpers.GetPipePath(PipeName);
+        public string Text => $"-i \"{PipePath}\"";
+
+        public InputExistingPipeArgument(string pipeName)
+        {
+            PipeName = pipeName;
+        }
+
+        public void Pre()
+        {
+            if (string.IsNullOrEmpty(PipeName))
+            {
+                throw new InvalidOperationException("Pipe name cannot be null or empty");
+            }
+        }
+
+        public async Task During(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var tcs = new TaskCompletionSource<bool>();
+                cancellationToken.Register(() => tcs.TrySetCanceled());
+                await tcs.Task;
+            }
+            catch(OperationCanceledException)
+            {
+                Debug.WriteLine($"{GetType().Name} cancelled");
+            }
+        }
+
+        public void Post()
+        {
+        }
+    }
+}
