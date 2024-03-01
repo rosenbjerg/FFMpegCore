8a764eccc7e0ca37d9df2e0da8694a8dbdb2bdcc
AddyMills <74471839+AddyMills@users.noreply.github.com>
2024-03-01 07:08:16 -0600

    Add MultiInputArgument

---
+++ FFMpegCore/FFMpeg/Arguments/MultiInputArgument.cs
@@ -0,0 +1,47 @@
+﻿namespace FFMpegCore.Arguments
+{
+    /// <summary>
+    /// Represents input parameters for multiple files
+    /// </summary>
+    public class MultiInputArgument : IInputArgument
+    {
+        public readonly bool VerifyExists;
+        public readonly string[] FilePaths;
+
+        public MultiInputArgument(bool verifyExists, params string[] filePaths)
+        {
+            VerifyExists = verifyExists;
+            FilePaths = filePaths;
+        }
+
+        public MultiInputArgument(string[] filePaths, bool verifyExists) : this(verifyExists, filePaths) { }
+
+        public void Pre()
+        {
+            if (VerifyExists)
+            {
+                var missingFiles = new List<string>();
+                foreach (var filePath in FilePaths)
+                {
+                    if (!File.Exists(filePath))
+                    {
+                        missingFiles.Add(filePath);
+                    }
+                }
+
+                if (missingFiles.Any())
+                {
+                    throw new FileNotFoundException($"The following input files were not found: {string.Join(", ", missingFiles)}");
+                }
+            }
+        }
+
+        public Task During(CancellationToken cancellationToken = default) => Task.CompletedTask;
+        public void Post() { }
+
+        /// <summary>
+        /// Generates a combined input argument text for all file paths
+        /// </summary>
+        public string Text => string.Join(" ", FilePaths.Select(filePath => $"-i \"{filePath}\""));
+    }
+}
