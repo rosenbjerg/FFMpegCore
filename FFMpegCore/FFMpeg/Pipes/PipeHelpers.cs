94174a28dbf4e5b37872e2071207f0712c208746
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2025-10-17 19:47:01 +0200

    Include 16 guid chars in pipe name

--- FFMpegCore/FFMpeg/Pipes/PipeHelpers.cs
+++ FFMpegCore/FFMpeg/Pipes/PipeHelpers.cs
@@ -6,7 +6,7 @@ internal static class PipeHelpers
 {
     public static string GetUnqiuePipeName()
     {
-        return $"FFMpegCore_{Guid.NewGuid().ToString("N").Substring(0, 5)}";
+        return $"FFMpegCore_{Guid.NewGuid().ToString("N").Substring(0, 16)}";
     }
 
     public static string GetPipePath(string pipeName)
